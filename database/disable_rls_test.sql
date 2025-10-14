3c24071adcc58e6163b7c94f520f4fbf5b471e28
KurisuuChan <kurisuuuchannn@gmail.com>
2025-10-14 16:30:40 +0800

    delete unnecessary files

--- database/disable_rls_test.sql
+++
@@ -1,7 +0,0 @@
--- 🚨 DISABLE RLS TO TEST USER DELETION
--- Run this command in Supabase SQL Editor
-
-ALTER TABLE public.users DISABLE ROW LEVEL SECURITY;
-
--- After running this, go back to your app and try deleting a user
--- If it works, then RLS was the problem!
